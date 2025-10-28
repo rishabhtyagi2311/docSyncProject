bd2acc2b04062f3e0b72b97682ffe5dfce0807aa
Rishabh Tyagi <rishabh.tyagi2311@gmail.com>
2025-10-28 18:21:09 +0530

    db setup and schema creation

---
+++ server/prisma/migrations/20251028125012_user_and_maintainer_schema_created/migration.sql
@@ -0,0 +1,21 @@
+-- CreateTable
+CREATE TABLE "user" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "user_pkey" PRIMARY KEY ("id")
+);
+
+-- CreateTable
+CREATE TABLE "maintainer" (
+    "id" SERIAL NOT NULL,
+    "firstName" TEXT NOT NULL,
+    "lastName" TEXT NOT NULL,
+    "email" TEXT NOT NULL,
+    "password" TEXT NOT NULL,
+
+    CONSTRAINT "maintainer_pkey" PRIMARY KEY ("id")
+);
